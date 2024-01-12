ea9afe0e26304fa263f1c0d6946d37e4b165341e
Corey <Corey255A1@hotmail.com>
2024-01-12 11:47:51 -0500

    Committing initial working product

---
+++ FunKeyViewer/MainWindow.xaml.cs
@@ -0,0 +1,45 @@
+﻿using FunKeyViewer.ViewModel;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Windows.Threading;
+
+namespace FunKeyViewer
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindowViewModel ViewModel { get; set; }
+        public MainWindow()
+        {
+            ViewModel = new MainWindowViewModel(this);
+            DataContext = ViewModel;
+            InitializeComponent();
+        }
+
+        private void BorderMouseDown(object sender, MouseButtonEventArgs e)
+        {
+            DragMove();
+        }
+
+        private void CloseClick(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+    }
+}
---
+++ FunKeyViewer/ViewModel/MainWindowViewModel.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Threading;
+
+namespace FunKeyViewer.ViewModel
+{
+    public class MainWindowViewModel
+    {
+        private DispatcherTimer _itemRemoveTimer;
+        private KeyStateManager _keyStateManager;
+        public const int MAX_COMBO_HISTORY = 5;
+        public ObservableCollection<List<KeyState>> KeyHistory { get; private set; } = new();
+        public ObservableCollection<KeyState> CurrentKeys { get; private set; } = new();
+        public MainWindowViewModel(Window window)
+        {
+            _keyStateManager = new KeyStateManager(Dispatcher.CurrentDispatcher);
+            _keyStateManager.AllKeysReleased += KeyStateManagerAllKeysReleased;
+            _keyStateManager.KeyPressed += KeyStateManagerKeyPressed;
+            _itemRemoveTimer = new DispatcherTimer();
+            _itemRemoveTimer.Interval = TimeSpan.FromSeconds(2);
+            _itemRemoveTimer.Tick += ItemRemoveTimer;
+
+
+            window.SourceInitialized += WindowSourceInitialized;
+            window.Closed += WindowClosed;
+        }
+
+        private void KeyStateManagerKeyPressed(object? sender, KeyState e)
+        {
+            CurrentKeys.Add(e);
+        }
+
+        private void RemoveOldestKeyCombo()
+        {
+            if(KeyHistory.Count == 0) { return; }
+            KeyHistory.RemoveAt(0);
+        }
+
+        private void ItemRemoveTimer(object? sender, EventArgs e)
+        {
+            RemoveOldestKeyCombo();
+        }
+
+        private void KeyStateManagerAllKeysReleased(object? sender, EventArgs e)
+        {
+            KeyHistory.Add(new List<KeyState>(_keyStateManager.ComboKeys));
+            CurrentKeys.Clear();
+            _itemRemoveTimer.Stop();
+            _itemRemoveTimer.Start();
+            if (KeyHistory.Count > MAX_COMBO_HISTORY)
+            {
+                RemoveOldestKeyCombo();
+            }
+        }
+
+        private void WindowClosed(object? sender, EventArgs e)
+        {
+            _keyStateManager.StopThread();
+        }
+
+        private void WindowSourceInitialized(object? sender, EventArgs e)
+        {
+            _keyStateManager.StartThread();
+        }
+
+
+    }
+}
